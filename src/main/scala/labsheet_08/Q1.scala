3b38e372a72dba29bc823cecc48c08b0f78e8bf7
TharinduDakshina <tharindudakshina527@gmail.com>
2023-08-22 11:25:46 +0530

    insert labsheet 08

---
+++ src/main/scala/labsheet_08/Q1.scala
@@ -0,0 +1,19 @@
+package labsheet_08
+
+object Q1 {
+  def main(args: Array[String]): Unit = {
+    val calculateInterest: Double => Double = (depositAmount: Double) => {
+      depositAmount match {
+        case amount if amount <= 20000 => amount * 0.02
+        case amount if amount <= 200000 => amount * 0.04
+        case amount if amount <= 2000000 => amount * 0.035
+        case amount => amount * 0.065
+      }
+    }
+
+    val depositAmount = 5000.0
+    val interest = calculateInterest(depositAmount)
+    println(s"Interest earned: $interest")
+  }
+
+}
