e32964abf8ea7f74e17e1b3f3f61494c369424c8
TharinduDakshina <tharindudakshina527@gmail.com>
2023-08-29 22:53:18 +0530

    added labsheet 09

---
+++ src/main/scala/labsheet_10/Q2.scala
@@ -0,0 +1,12 @@
+package labsheet_10
+
+object Q2 {
+  def main(args: Array[String]): Unit = {
+    var fruit = List("apple", "banana", "cherry", "date")
+    countwords(fruit)
+  }
+
+  def countwords(value: List[String]) = {
+    println("Total count of letter occurrences: " + value.map(num => num.length).reduce((x, y) => x + y))
+  }
+}
