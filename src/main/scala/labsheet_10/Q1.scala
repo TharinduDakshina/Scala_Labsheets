1bf1ff1386721e14c5dd1d07d6d29f5346bb5263
TharinduDakshina <tharindudakshina527@gmail.com>
2023-09-01 13:34:46 +0530

    update content

--- src/main/scala/labsheet_10/Q1.scala
+++ src/main/scala/labsheet_10/Q1.scala
@@ -3,16 +3,17 @@ package labsheet_10
 object Q1 {
   def main(args: Array[String]): Unit = {
     var cel = List(0, 10, 20, 30)
-    celtofar(cel)
+    calFahrenhite(cel)
 
   }
 
-  def celtofar(cel: List[Int]) = {
+  def calFahrenhite(cel: List[Int]) = {
     var far = cel.map(temp => (temp * 9 / 5) + 32)
-    println("Average Fahrenheit temperature: " + average(far))
+    println(far);
+    println("Average Fahrenheit temperature: " + avg(far))
   }
 
-  def average(far: List[Int]): Double = {
+  def avg(far: List[Int]): Double = {
     far.reduce((x, y) => x + y) / far.length
 
   }
